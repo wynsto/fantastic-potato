1926e89793d1cf6b0dc36284912034f08e30e8db
Wynston Hou <houyewei@gmail.com>
2024-05-18 15:51:26 +0800

    server

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -5,7 +5,19 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <boost/asio.hpp>
+#include <array>
+#include <ctime>
 
+using boost::asio::ip::tcp;
+
+
+std::string make_daytime_string()
+{
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
 
 auto main(int argc, char** argv) -> int {
   const std::unordered_map<std::string, fantastic_potato::LanguageCode> languages{
@@ -55,6 +67,28 @@ auto main(int argc, char** argv) -> int {
   std::string resp = potato.get(url);
 
   std::cout << resp << std::endl;
+
+  try
+  {
+    boost::asio::io_context io_context;
+
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+
+    for (;;)
+    {
+      tcp::socket socket(io_context);
+      acceptor.accept(socket);
+
+      std::string message = make_daytime_string();
+
+      boost::system::error_code ignored_error;
+      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
 
 
   return 0;
